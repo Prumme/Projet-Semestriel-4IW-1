08ef74b18ad7228f6ff1cb93db0bfc1941c7ab92
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2023-10-26 17:25:56 +0200

    :sparkles: Add a component nav and the tailwind config for our colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        100: "#49AB9A",
+        200: "#3E9E8D",
+        300: "#338180",
+        400: "#287473",
+        500: "#1E6766",
+      },
+      secondary: "#0A1B3A",
+      white: "#F5F5F5",
+      black: "#1E1E1E",
+    },
   },
   plugins: [],
 };
