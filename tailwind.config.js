d2d455586e87926e0d149bd869ee0e07a1a646bb
Robin Fauchery <robin.fauchery.rf@gmail.com>
2023-11-13 22:08:46 +0100

    Adding Colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/**/*.js","./assets/**/*.scss", "./templates/**/*.html.twig"],
+  content: [
+    "./assets/**/*.js",
+    "./assets/**/*.scss",
+    "./templates/**/*.html.twig",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#49AB9A",
-          200: "#3E9E8D",
-          300: "#62BCAC",
-          400: "#287473",
-          500: "#337D70",
-          600: "#3D8E80",
-        },
-        secondary: "#0A1B3A",
-        secondaryLight: "#21295C",
-        white: "#F5F5F5",
+        primary: "#49AB9A",
+        secondary: "#1E143B",
+        white: "#F2F4F8",
         black: "#1E1E1E",
+        secondary_light: "#21295C",
+        primary_darken: "#337D70",
+        danger: "#F34E29",
+        warning: "#FFD339",
+        disabled: "#BEDFD9",
       },
     },
   },
-  plugins: [
-      require("@tailwindcss/forms"),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
