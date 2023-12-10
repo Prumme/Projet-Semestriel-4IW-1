d1a3408cb938066237d4419f62a6e9b877dbab4e
Axel <axel77g@hotmail.com>
2023-12-10 13:56:53 +0100

    [COMPANY USER] create table

---
+++ src/Table/UserCompanyTable.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Table;
+
+use App\Table\Core\Table;
+
+class UserCompanyTable extends Table{
+    protected array $neededData = ['company'];
+    protected function buildTable() : void
+    {
+        $company = $this->data['company'];
+
+        $this->setHeaders([
+            [
+                'title' => 'Identity',
+                'component' => 'company_user/identity_cell.html.twig',
+                'key' => 'email',
+            ],
+            [
+                'title' => 'Roles',
+                'component' => 'company_user/roles_cell.html.twig',
+                'key' => 'roles',
+            ]
+        ]);
+
+        $this->setItemsActions([
+            [
+                'content'=>'Show',
+                'icon'=>'eye',
+                "href"=> [
+                    'path'=>'app_company_user_show',
+                    'params'=>[
+                        'id'=> fn($item)=>$item->getId(),
+                        'company'=> $company->getId(),
+                    ]
+                ],
+            ],
+            [
+                'content'=>'Edit',
+                'icon'=>'create',
+                "href"=> [
+                    'path'=>'app_company_user_edit',
+                    'params'=>[
+                        'id'=> fn($item)=>$item->getId(),
+                        'company'=> $company->getId(),
+                    ]
+                ],
+            ],
+        ]);
+
+        $this->setGroupedActions([
+            [
+                'content'=>"delete",
+                'icon'=>'trash',
+                'href'=>[
+                    'path'=>'app_company_user_mass_delete',
+                    'params'=>[
+                        'company'=> $company->getId(),
+                    ]
+                ],
+            ]
+        ]);
+
+        $this->setActions([
+            [
+                'content'=>"Add user",
+                'icon'=>'add-circle-outline',
+                'href'=>[
+                    'path'=>'app_company_user_new',
+                    'params'=>[
+                        'company'=> $company->getId(),
+                    ]
+                ],
+            ]
+        ]);
+    }
+}
