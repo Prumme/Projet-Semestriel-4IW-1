be95acc17e1e110a81f2a864d4e89910dafe04f7
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2023-12-20 16:44:25 +0100

    [CUSTOMER] : CRUD pages for the customer

--- src/Table/CustomersTable.php
+++ src/Table/CustomersTable.php
@@ -4,9 +4,10 @@ namespace App\Table;
 
 use App\Table\Core\Table;
 
-class CustomersTable extends Table{
+class CustomersTable extends Table
+{
     protected array $neededData = ['company'];
-    protected function buildTable() : void
+    protected function buildTable(): void
     {
         $company = $this->data['company'];
 
@@ -17,49 +18,38 @@ class CustomersTable extends Table{
                 'key' => 'lastname',
             ],
             [
-                'title'=>"Company",
-                'component'=>'customer/company_cell.html.twig',
-                'key'=>'companyName',
+                'title' => "Company",
+                'component' => 'customer/company_cell.html.twig',
+                'key' => 'companyName',
             ],
             [
-                'title'=>"Phone",
-                'key'=>'tel'
+                'title' => "Phone",
+                'key' => 'tel'
             ]
         ]);
 
         $this->setItemsActions([
             [
-                'content'=>'Show',
-                'icon'=>'eye',
-                "href"=> [
-                    'path'=>'app_customer_show',
-                    'params'=>[
-                        'id'=> fn($item)=>$item->getId(),
-                        'company'=> $company->getId(),
+                'content' => 'Edit',
+                'icon' => 'create',
+                "href" => [
+                    'path' => 'app_customer_edit',
+                    'params' => [
+                        'id' => fn ($item) => $item->getId(),
+                        'company' => $company->getId(),
                     ]
                 ],
             ],
             [
-                'content'=>'Edit',
-                'icon'=>'create',
-                "href"=> [
-                    'path'=>'app_customer_edit',
-                    'params'=>[
-                        'id'=> fn($item)=>$item->getId(),
-                        'company'=> $company->getId(),
-                    ]
-                ],
-            ],
-            [
-                'content'=>'Delete',
-                'icon'=>'trash',
-                "href"=> [
-                    'csrf'=> fn($item)=> 'delete' . $item->getId(),
-                    'method'=>'post',
-                    'path'=>'app_customer_delete',
-                    'params'=>[
-                        'id'=> fn($item)=>$item->getId(),
-                        'company'=> $company->getId(),
+                'content' => 'Delete',
+                'icon' => 'trash',
+                "href" => [
+                    'csrf' => fn ($item) => 'delete' . $item->getId(),
+                    'method' => 'post',
+                    'path' => 'app_customer_delete',
+                    'params' => [
+                        'id' => fn ($item) => $item->getId(),
+                        'company' => $company->getId(),
                     ]
                 ],
             ],
@@ -68,12 +58,12 @@ class CustomersTable extends Table{
 
         $this->setActions([
             [
-                'content'=>"Add customer",
-                'icon'=>'add-circle-outline',
-                'href'=>[
-                    'path'=>'app_customer_new',
-                    'params'=>[
-                        'company'=> $company->getId(),
+                'content' => "Add customer",
+                'icon' => 'add-circle-outline',
+                'href' => [
+                    'path' => 'app_customer_new',
+                    'params' => [
+                        'company' => $company->getId(),
                     ]
                 ],
             ]
