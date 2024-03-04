fc1c08cf5aa1a8026d100903461c9b83fadb13f2
Axel <axel77g@hotmail.com>
2024-03-04 19:35:05 +0100

    remove profile picture

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Security\AuthentificableRoles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -15,17 +16,35 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('profilePictureFile', VichImageType::class, [
+        /*->add('profilePictureFile', VichImageType::class, [
             'label' => '',
             'required' => false,
             'allow_delete' => true,
             'delete_label' => 'Remove profile picture',
             'download_uri' => true,
             'image_uri' => true,
+        ])*/
+            ->add('email',EmailType::class,[
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'john.doe@email.com',
+                    'icon'=>'at'
+                ]
         ])
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname',null,[
+                'label' => 'First Name',
+                'attr' => [
+                    'placeholder' => 'John',
+                    'icon'=>'user'
+                ]
+            ])
+            ->add('lastname',null,[
+                'label' => 'Last Name',
+                'attr' => [
+                    'placeholder' => 'Doe',
+                    'icon'=>'user'
+                ]
+            ])
             ->add('theme', ChoiceType::class, [
                 'choices' => [
                     'Green' => 'default',
